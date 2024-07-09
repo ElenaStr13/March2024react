c137bc8ab7a3e59f84a567d3ee5b6c42f90e89fa
ElenaStr13 <115697145>
2024-07-09 21:42:33 +0300

    homework 6

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,5 +1,6 @@
 import { pluginTypedCSSModules } from '@rsbuild/plugin-typed-css-modules';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     plugins: [pluginTypedCSSModules()],
 };
