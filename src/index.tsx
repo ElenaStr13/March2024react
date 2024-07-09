36524ed418e0ebe6a62241fbc388944507c387b3
ElenaStr13 <115697145>
2024-07-09 19:12:12 +0300

    homework 6

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {RouterProvider} from "react-router-dom";
+import * as router from "./router";
+
+import './index.css';
+
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <RouterProvider router={router}/>
+);
+
+
